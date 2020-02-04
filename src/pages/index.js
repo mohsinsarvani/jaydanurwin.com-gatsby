80035b64b68f2567ccef635bb0b64faef31e1e0a
Jaydan Urwin <jaydan.urwin@gmail.com>
2020-02-03 23:36:00 -0700

    added in icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import jaydanProfile from "../images/profile-image.jpg"
+import youtubeSocialIcon from "../images/icons/youtube-icon-24px.svg"
+import instagramSocialIcon from "../images/icons/instagram-glyph-24px.svg"
+import twitterSocialIcon from "../images/icons/twitter-icon-24px.svg"
+import githubSocialIcon from "../images/icons/github-icon-24px.svg"
 import jaydanUrwinLogo from '../images/jaydan-urwin-logo.svg'
+
 import "../styles/app.scss"
 
 const IndexPage = () => (
     <section className="jaydan-home__section">
-        <img src="./images/profile-image.jpg" className="jaydan-home-profile__img" />
+        <img src={jaydanProfile} className="jaydan-home-profile__img" />
         <div className="jaydan-home-bio__container">
             <h2>
                 It’s hard to sum up a person in a bio isn’t it? Follow me if you want
@@ -16,30 +22,30 @@ const IndexPage = () => (
             > --> */}
       </div>
         <div className="jaydan-home-social__container">
-            {/* <a href="https://www.youtube.com/channel/UCS1dnjhkW88IM5u6GN5jNvA">
+            <a href="https://www.youtube.com/channel/UCS1dnjhkW88IM5u6GN5jNvA">
                 <img
-                    src="./images/icons/youtube-icon-24px.svg"
+                    src={youtubeSocialIcon}
                     alt="YouTube Social Link"
                 />
             </a>
             <a href="https://instagram.com/jaydanurwin">
                 <img
-                    src="./images/icons/instagram-glyph-24px.svg"
+                    src={instagramSocialIcon}
                     alt="Instagram Social Icon"
                 />
             </a>
             <a href="https://twitter.com/jaydanurwin">
                 <img
-                    src="./images/icons/twitter-icon-24px.svg"
+                    src={twitterSocialIcon}
                     alt="Twitter Social Icon"
                 />
             </a>
             <a href="https://github.com/jaydanurwin">
                 <img
-                    src="./images/icons/github-icon-24px.svg"
+                    src={githubSocialIcon}
                     alt="Github Social Icon"
                 />
-            </a> */}
+            </a>
         </div>
         <img
             src={jaydanUrwinLogo}
